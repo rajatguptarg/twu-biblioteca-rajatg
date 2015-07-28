b47e50464e371b8b419ec5e34e439ca4910bbca3
Rajat Gupta <rajat.gupta712@gmail.com>
2015-07-28 09:09:06 +0530

    |#0000| Added Initial Boiler Plate of project

---
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+public class BibliotecaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
