cf9bde4b71bcac3b0a6d5ad08b0223dd4f3c87e0
Rajat Gupta <rajat.gupta712@gmail.com>
2015-07-28 11:55:55 +0530

    |#0001| Modified Menu specs to use mockito instead of byte array output stream

--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,34 +1,18 @@
 package com.twu.biblioteca;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.junit.Assert.*;
+import org.mockito.Mockito;
 
 
 public class MenuTest {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(System.out);
-    }
 
     @Test
-    public void shouldDisplayTheWelcomeMessage() {
-        Menu menu = new Menu();
-        String expectedWelcomeMessage = "HELLO..!! WELCOME TO BIBLIOTECA..!!\n";
+    public void menuShouldDisplayTheWelcomeMessage() {
+        Menu menu = Mockito.mock(Menu.class);
 
         menu.displayWelcomeMessage();
 
-        assertEquals(expectedWelcomeMessage, outContent.toString());
+        Mockito.verify(menu).displayWelcomeMessage();
+
     }
 }
