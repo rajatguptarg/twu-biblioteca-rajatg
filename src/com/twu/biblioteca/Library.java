ff9aa87d8be19ae66dd1821da61fa847a9546838
Rajat Gupta <rajat.gupta712@gmail.com>
2015-07-28 12:25:42 +0530

    |#0002| Added Library to dispaly list of all the books

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,4 +1,14 @@
 package com.twu.biblioteca;
 
+
 public class Library {
+    private Book[] listOfBooks;
+
+    public Library(Book[] listOfBooks) {
+        this.listOfBooks = listOfBooks;
+    }
+
+    public Book[] getBooks() {
+        return listOfBooks;
+    }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+
 public class LibraryTest {
+
+    @Test
+    public void shouldDisplayTheListOfBooks() {
+        Book book = new Book("My Book", "Rajat", "2010");
+        Library library = new Library(new Book[]{book});
+
+        assertArrayEquals(new Book[]{book}, library.getBooks());
+    }
 }
