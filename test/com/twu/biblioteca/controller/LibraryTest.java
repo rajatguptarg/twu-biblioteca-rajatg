afc61761b528afa613cc4018cc9282e6616afc69
Rajat Gupta <rajat.gupta712@gmail.com>
2015-08-03 22:54:42 +0530

    |#0044| Added new specs for the Library

--- test/com/twu/biblioteca/controller/LibraryTest.java
+++ test/com/twu/biblioteca/controller/LibraryTest.java
@@ -1,6 +1,11 @@
 package com.twu.biblioteca.controller;
 
+import com.twu.biblioteca.model.Book;
+import com.twu.biblioteca.model.LibraryItem;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,7 +13,56 @@ import static org.junit.Assert.*;
 public class LibraryTest {
 
     @Test
-    public void testName() {
-        assertEquals(true, true);
+    public void shouldReturnListOfAvailableItems() {
+        List<LibraryItem> availableItems = new ArrayList<LibraryItem>();
+        List<LibraryItem> checkedOutItem = new ArrayList<LibraryItem>();
+        availableItems.add(new Book("My Book", "Rajat", "2012"));
+        Library library = new Library(availableItems, checkedOutItem);
+
+        assertEquals(1, library.listAvailableItems().size());
+    }
+
+    @Test
+    public void shouldReturnListOfCheckedOutItems() {
+        List<LibraryItem> availableItems = new ArrayList<LibraryItem>();
+        List<LibraryItem> checkedOutItem = new ArrayList<LibraryItem>();
+        availableItems.add(new Book("My Book", "Rajat", "2012"));
+        Library library = new Library(availableItems, checkedOutItem);
+
+        library.performCheckOut("my book");
+
+        assertEquals(1, library.listCheckedOutItems().size());
+    }
+
+    @Test
+    public void shouldBeAbleToPerformCheckOut() {
+        List<LibraryItem> availableItems = new ArrayList<LibraryItem>();
+        List<LibraryItem> checkedOutItem = new ArrayList<LibraryItem>();
+        availableItems.add(new Book("My Book", "Rajat", "2012"));
+        Library library = new Library(availableItems, checkedOutItem);
+
+        assertEquals(true, library.performCheckOut("my book"));
+    }
+
+    @Test
+    public  void shouldBeAbleToReturnBook() {
+        List<LibraryItem> availableItems = new ArrayList<LibraryItem>();
+        List<LibraryItem> checkedOutItem = new ArrayList<LibraryItem>();
+        availableItems.add(new Book("My Book", "Rajat", "2012"));
+        Library library = new Library(availableItems, checkedOutItem);
+
+        library.performCheckOut("my book");
+
+        assertEquals(true, library.performReturn("my book"));
+    }
+
+    @Test
+    public  void shouldBeAbleToNotReturnBookIfNotCheckedOut() {
+        List<LibraryItem> availableItems = new ArrayList<LibraryItem>();
+        List<LibraryItem> checkedOutItem = new ArrayList<LibraryItem>();
+        availableItems.add(new Book("My Book", "Rajat", "2012"));
+        Library library = new Library(availableItems, checkedOutItem);
+
+        assertEquals(false, library.performReturn("my book"));
     }
 }
