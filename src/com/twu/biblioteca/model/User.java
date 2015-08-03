7ac6a9691590dfe96d001366b22bfd7658fad0c1
Rajat Gupta <rajat.gupta712@gmail.com>
2015-08-03 14:32:05 +0530

    |#0041| User is now able to tell its role

--- src/com/twu/biblioteca/model/User.java
+++ src/com/twu/biblioteca/model/User.java
@@ -3,10 +3,12 @@ package com.twu.biblioteca.model;
 public class User {
     private String libraryNumber;
     private String password;
+    private String role;
 
-    public User(String libraryNumber, String password) {
+    public User(String libraryNumber, String password, String role) {
         this.libraryNumber = libraryNumber;
         this.password = password;
+        this.role = role;
     }
 
     public String getLibraryNumber() {
@@ -15,5 +17,9 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
--- test/com/twu/biblioteca/model/UserTest.java
+++ test/com/twu/biblioteca/model/UserTest.java
@@ -9,15 +9,29 @@ public class UserTest {
 
     @Test
     public void shouldBeAbleToTellLibraryNumber() {
-        User user = new User("123-1234", "12345");
+        User user = new User("123-1234", "12345", "user");
 
         assertEquals("123-1234", user.getLibraryNumber());
     }
 
     @Test
     public void shouldBeAbleToTellPassword() {
-        User user = new User("123-1234", "12345");
+        User user = new User("123-1234", "12345", "user");
 
         assertEquals("12345", user.getPassword());
+    }
+
+    @Test
+    public void shouldBeAbleToTellItsRoleIfItIsNormalUser() {
+        User user = new User("123-1234", "12345", "user");
+
+        assertEquals("user", user.getRole());
+    }
+
+    @Test
+    public void shouldBeAbleToTellItsRoleIfItIsAdmin() {
+        User user = new User("123-1234", "12345", "admin");
+
+        assertEquals("admin", user.getRole());
     }
 }
